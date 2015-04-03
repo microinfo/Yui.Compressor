f3358cd88e3a36c77b759b56f73c89da91c2a515
XuTongle <xutongle@gmail.com>
2015-04-03 11:09:12 +0800

    初始导入
    Signed-off-by: XuTongle <xutongle@gmail.com>

---
+++ Main.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace TintSoft
+{
+    partial class Main
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSelect = new System.Windows.Forms.Button();
+            this.btnLoad = new System.Windows.Forms.Button();
+            this.btnCompressor = new System.Windows.Forms.Button();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.txtInfo = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cbEncoder = new System.Windows.Forms.ComboBox();
+            this.linkadmin5 = new System.Windows.Forms.LinkLabel();
+            this.ListFile = new System.Windows.Forms.ListView();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.SuspendLayout();
+            // 
+            // btnSelect
+            // 
+            this.btnSelect.Location = new System.Drawing.Point(292, 11);
+            this.btnSelect.Name = "btnSelect";
+            this.btnSelect.Size = new System.Drawing.Size(50, 23);
+            this.btnSelect.TabIndex = 0;
+            this.btnSelect.Text = "...";
+            this.btnSelect.UseVisualStyleBackColor = true;
+            this.btnSelect.Click += new System.EventHandler(this.btnSelect_Click);
+            // 
+            // btnLoad
+            // 
+            this.btnLoad.Location = new System.Drawing.Point(348, 10);
+            this.btnLoad.Name = "btnLoad";
+            this.btnLoad.Size = new System.Drawing.Size(75, 23);
+            this.btnLoad.TabIndex = 1;
+            this.btnLoad.Text = "加  载";
+            this.btnLoad.UseVisualStyleBackColor = true;
+            this.btnLoad.Click += new System.EventHandler(this.btnLoad_Click);
+            // 
+            // btnCompressor
+            // 
+            this.btnCompressor.Location = new System.Drawing.Point(429, 11);
+            this.btnCompressor.Name = "btnCompressor";
+            this.btnCompressor.Size = new System.Drawing.Size(75, 23);
+            this.btnCompressor.TabIndex = 2;
+            this.btnCompressor.Text = "压 缩";
+            this.btnCompressor.UseVisualStyleBackColor = true;
+            this.btnCompressor.Click += new System.EventHandler(this.btnCompressor_Click);
+            // 
+            // txtPath
+            // 
+            this.txtPath.Enabled = false;
+            this.txtPath.Location = new System.Drawing.Point(12, 12);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(274, 21);
+            this.txtPath.TabIndex = 3;
+            // 
+            // txtInfo
+            // 
+            this.txtInfo.Enabled = false;
+            this.txtInfo.Location = new System.Drawing.Point(12, 39);
+            this.txtInfo.Name = "txtInfo";
+            this.txtInfo.Size = new System.Drawing.Size(492, 21);
+            this.txtInfo.TabIndex = 4;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 360);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 12);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "编码类型：";
+            // 
+            // cbEncoder
+            // 
+            this.cbEncoder.FormattingEnabled = true;
+            this.cbEncoder.Items.AddRange(new object[] {
+            "UTF-8",
+            "ASCII",
+            "Default",
+            "Unicode"});
+            this.cbEncoder.Location = new System.Drawing.Point(83, 356);
+            this.cbEncoder.Name = "cbEncoder";
+            this.cbEncoder.Size = new System.Drawing.Size(75, 20);
+            this.cbEncoder.TabIndex = 6;
+            this.cbEncoder.Text = "UTF-8";
+            // 
+            // linkadmin5
+            // 
+            this.linkadmin5.AutoSize = true;
+            this.linkadmin5.Location = new System.Drawing.Point(367, 360);
+            this.linkadmin5.Name = "linkadmin5";
+            this.linkadmin5.Size = new System.Drawing.Size(143, 12);
+            this.linkadmin5.TabIndex = 7;
+            this.linkadmin5.TabStop = true;
+            this.linkadmin5.Text = "http://www.tintsoft.com";
+            this.linkadmin5.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkadmin5_LinkClicked);
+            // 
+            // ListFile
+            // 
+            this.ListFile.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4});
+            this.ListFile.FullRowSelect = true;
+            this.ListFile.GridLines = true;
+            this.ListFile.HideSelection = false;
+            this.ListFile.Location = new System.Drawing.Point(12, 66);
+            this.ListFile.MultiSelect = false;
+            this.ListFile.Name = "ListFile";
+            this.ListFile.ShowGroups = false;
+            this.ListFile.Size = new System.Drawing.Size(492, 284);
+            this.ListFile.TabIndex = 5;
+            this.ListFile.UseCompatibleStateImageBehavior = false;
+            this.ListFile.View = System.Windows.Forms.View.Details;
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = "文件";
+            this.columnHeader1.Width = 228;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "原大小";
+            this.columnHeader2.Width = 100;
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "压缩后大小";
+            this.columnHeader3.Width = 100;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "状态";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(516, 383);
+            this.Controls.Add(this.ListFile);
+            this.Controls.Add(this.linkadmin5);
+            this.Controls.Add(this.cbEncoder);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtInfo);
+            this.Controls.Add(this.txtPath);
+            this.Controls.Add(this.btnCompressor);
+            this.Controls.Add(this.btnLoad);
+            this.Controls.Add(this.btnSelect);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.MaximizeBox = false;
+            this.Name = "Main";
+            this.Text = "Yui.Compressor -- By 天智软件";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSelect;
+        private System.Windows.Forms.Button btnLoad;
+        private System.Windows.Forms.Button btnCompressor;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.TextBox txtInfo;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cbEncoder;
+        private System.Windows.Forms.LinkLabel linkadmin5;
+        private System.Windows.Forms.ListView ListFile;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+    }
+}
+
---
+++ Main.cs
@@ -0,0 +1,267 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.IO;
+using System.Diagnostics;
+using System.Globalization;
+using Yahoo.Yui.Compressor;
+using System.Windows.Forms;
+
+namespace TintSoft
+{
+    public partial class Main : Form
+    {
+        private int jsCount;
+        private int cssCount;
+        private string strFilePath = "";
+        private IList<FileInfo> fileList = new List<FileInfo>();
+
+        public Main()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// 选择文件夹
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnSelect_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog dialog = new FolderBrowserDialog();
+            dialog.Description = "请选择文件路径";
+            if (dialog.ShowDialog() == DialogResult.OK)
+            {
+                this.txtPath.Text = dialog.SelectedPath;
+            }
+        }
+
+        /// <summary>
+        /// 加载文件
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnLoad_Click(object sender, EventArgs e)
+        {
+            if (!Directory.Exists(this.txtPath.Text))
+            {
+                MessageBox.Show("路径不存在，请重新选择！");
+                this.btnSelect.PerformClick();
+            }
+            else
+            {
+                this.strFilePath = this.txtPath.Text;
+                this.jsCount = 0;
+                this.cssCount = 0;
+                this.fileList.Clear();
+                this.ListFile.Items.Clear();
+                this.GetDirList(new DirectoryInfo(this.txtPath.Text));
+                if (this.fileList.Count == 0)
+                { 
+                    this.btnCompressor.Enabled = false;
+                    MessageBox.Show("未找到js或css文件！");
+                }
+                else
+                {
+                    this.txtInfo.Text = "共加载了" + this.jsCount.ToString() + "个JS文件、" + this.cssCount.ToString() + "个CSS文件！";
+                    this.ListFile.BeginUpdate();
+                    foreach (FileInfo info in this.fileList)
+                    {
+                        ListViewItem item = new ListViewItem(info.FullName.Replace(this.txtPath.Text, ""), 0);
+                        item.SubItems.Add(this.FormatSize(info.Length));
+                        item.SubItems.Add("");
+                        item.SubItems.Add("就绪");
+                        this.ListFile.Items.Add(item);
+                    }
+                    this.ListFile.EndUpdate();
+                    this.btnCompressor.Enabled = true;
+                }
+            }
+        }
+
+        /// <summary>
+        /// 压缩
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void btnCompressor_Click(object sender, EventArgs e)
+        {
+            if (this.ListFile.Items.Count != 0)
+            {
+                Encoding encoding = this.GetEncoding();
+                int num = 0;
+                long num2 = 0L;
+                long num3 = 0L;
+                for (int i = 0; i < this.fileList.Count; i++)
+                {
+                    try
+                    {
+                        FileInfo finfo = this.fileList[i];
+                        //读取文件内容
+                        string javaScript = File.ReadAllText(finfo.FullName, encoding);
+                        if (!finfo.Name.Contains(".min."))
+                        {
+                            //取消文件只读
+                            File.SetAttributes(finfo.FullName, FileAttributes.Normal);
+                            if (finfo.Extension.ToLower() == ".js")
+                            {
+                                //初始化JS压缩类
+                                var js = new JavaScriptCompressor();
+                                js.CompressionType = CompressionType.Standard;//压缩类型
+                                js.Encoding = encoding;//编码
+                                js.IgnoreEval = false;//大小写转换
+                                js.ThreadCulture = System.Globalization.CultureInfo.CurrentCulture;
+                                //压缩该js
+                                javaScript = js.Compress(javaScript);
+                            }
+                            else if (finfo.Extension.ToLower() == ".css")
+                            {
+                                //进行CSS压缩
+                                CssCompressor css = new CssCompressor();
+                                javaScript = css.Compress(javaScript);
+                            }
+
+                            string filePath = finfo.FullName.Replace(finfo.Name, "");
+                            // 没有扩展名的文件名 “Default”
+                            string fimeName = filePath + Path.GetFileNameWithoutExtension(finfo.FullName) + ".min" + finfo.Extension.ToLower(); ;
+                            //写入文件内容
+                            File.WriteAllText(fimeName, javaScript);
+                            this.ListFile.Items[i].ForeColor = Color.Blue;
+                            this.ListFile.Items[i].SubItems[2].Text = this.FormatSize((long)javaScript.Length);
+                            this.ListFile.Items[i].SubItems[3].Text = "完成";
+                            num2 += finfo.Length;
+                            num3 += javaScript.Length;
+                        }
+                        else //跳过
+                        {
+                            this.ListFile.Items[i].SubItems[2].Text = this.FormatSize((long)javaScript.Length);
+                            this.ListFile.Items[i].SubItems[3].Text = "跳过";
+                            num2 += finfo.Length;
+                            num3 += javaScript.Length;
+                        }
+                    }
+                    catch (Exception exception)
+                    {
+                        this.ListFile.Items[i].ForeColor = Color.Red;
+                        this.ListFile.Items[i].SubItems[2].Text = "0";
+                        this.ListFile.Items[i].SubItems[3].Text = "错误:" + exception.Message;
+                        num++;
+                    }
+                    int num5 = i + 1;
+                    this.txtInfo.Text = "共" + this.fileList.Count.ToString() + "个文件！正处理第" + num5.ToString() + "个文件！";
+                    Application.DoEvents();
+                }
+                string text = "压缩完成！\r\n";
+                if (num > 0)
+                {
+                    text = text + num.ToString() + "个文件压缩发生错误！\r\n";
+                }
+                text = text + "总体压缩：";
+                text = text + ((((1.0 * num3) / ((double)num2)) * 100.0)).ToString() + "%";
+                this.txtInfo.Text = text.Replace("\r\n", " ");
+                if (num > 0)
+                {
+                    MessageBox.Show(text, "错误", MessageBoxButtons.OK, MessageBoxIcon.Hand);
+                }
+                else
+                {
+                    MessageBox.Show(text, "成功", MessageBoxButtons.OK, MessageBoxIcon.Asterisk);
+                }
+                //Process.Start(fileinfo.FullName);
+            }
+            else 
+            {
+                MessageBox.Show("请选择要压缩的路径！");
+            }
+        }
+
+        private string FormatSize(long size)
+        {
+            string str = "";
+            double num = (size * 1.0) * ((size < 0L) ? ((double)(-1)) : ((double)1));
+            if (num < 1024.0)
+            {
+                str = str + num.ToString() + "B";
+            }
+            else if (num < 1048576.0)
+            {
+                num /= 1024.0;
+                str = str + num.ToString("0.##") + "K";
+            }
+            else if (num < 1073741824.0)
+            {
+                num = (num / 1024.0) / 1024.0;
+                str = str + num.ToString("0.##") + "M";
+            }
+            else
+            {
+                str = str + ((((num / 1024.0) / 1024.0) / 1024.0)).ToString("0.##") + "G";
+            }
+            if (size < 0L)
+            {
+                str = "-" + str;
+            }
+            return str;
+        }
+
+        private void GetDirList(DirectoryInfo dir)
+        {
+            foreach (FileInfo info in dir.GetFiles("*.js"))
+            {
+                this.jsCount++;
+                this.fileList.Add(info);
+            }
+            foreach (FileInfo info2 in dir.GetFiles("*.css"))
+            {
+                this.cssCount++;
+                this.fileList.Add(info2);
+            }
+            DirectoryInfo[] directories = dir.GetDirectories();
+            if (directories.Length > 0)
+            {
+                foreach (DirectoryInfo info3 in directories)
+                {
+                    this.GetDirList(info3);
+                }
+            }
+        }
+
+        private Encoding GetEncoding()
+        {
+            Encoding encoding = Encoding.Default;
+            string str = this.cbEncoder.SelectedItem.ToString();
+            if (str == null)
+            {
+                return encoding;
+            }
+            if (!(str == "UTF8"))
+            {
+                if (str != "ASCII")
+                {
+                    if (str == "Default")
+                    {
+                        return Encoding.Default;
+                    }
+                    if (str != "Unicode")
+                    {
+                        return encoding;
+                    }
+                    return Encoding.Unicode;
+                }
+            }
+            else
+            {
+                return Encoding.UTF8;
+            }
+            return Encoding.ASCII;
+        }
+
+        private void linkadmin5_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            Process.Start("http://www.tintsoft.com");
+        }
+    }
+}
