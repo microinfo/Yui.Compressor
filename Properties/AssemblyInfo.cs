5e8444c5b60633e7d680341c5d34d71e0c59b7c4
XuTongle <xutongle@gmail.com>
2015-04-03 11:22:01 +0800

    update
    Signed-off-by: XuTongle <xutongle@gmail.com>

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,13 +5,13 @@ using System.Runtime.InteropServices;
 // 有关程序集的常规信息通过以下
 // 特性集控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("JSCSS")]
+[assembly: AssemblyTitle("Yui.Compressor")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("JSCSS")]
-[assembly: AssemblyCopyright("Copyright ©  2013")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCompany("Jinan TintSoft Co., Ltd")]
+[assembly: AssemblyProduct("Yui.Compressor")]
+[assembly: AssemblyCopyright("Copyright ©  2015")]
+[assembly: AssemblyTrademark("TintSoft")]
 [assembly: AssemblyCulture("")]
 
 // 将 ComVisible 设置为 false 使此程序集中的类型
